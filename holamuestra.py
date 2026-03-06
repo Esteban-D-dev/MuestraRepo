e088d6ad21d68ca3e0f5d06d7c9c405246a94138
Esteban <ecabrera@cajamorelia.com.mx>
2026-03-06 12:24:08 -0600

    otro cambio

--- holamuestra.py
+++ holamuestra.py
@@ -5,6 +5,9 @@ print("Esta es una muestra de python en el repositorio de github")
 print("Espero que les guste esta muestra de python")
 print("Gracias por ver esta muestra de python")
 print("Hasta luego")
+print("Adios")
+
+
 
 
 
